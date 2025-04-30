061bc6c7f464cc993a22be760b81f4fcb5584858
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-30 09:01:25 -0300

    06

--- ex-06/Program.cs
+++ ex-06/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int numero = -1;
+
+        while (numero >= -10)
+        {
+            Console.WriteLine(numero);
+            numero = numero - 1;
+        }
+    }
+}
