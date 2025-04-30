fe43f370313c3620e13f04f2db92a0c5c97142c3
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-30 08:56:28 -0300

    02

---
+++ ex-02/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número N:");
+        int N = int.Parse(Console.ReadLine());
+
+        int contador = 1;
+
+        while (contador <= N)
+        {
+            Console.WriteLine(contador);
+            contador = contador + 1;
+        }
+    }
+}
---
+++ ex-04/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int numero = 2;
+
+        while (numero <= N)
+        {
+            Console.WriteLine(numero);
+            numero = numero + 2;
+        }
+    }
+}
