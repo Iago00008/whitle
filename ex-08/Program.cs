94dee95a0128464e3b1c5e470ef2b8f1a62cacb7
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-30 09:07:56 -0300

    08

--- ex-08/Program.cs
+++ ex-08/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int resultado = 0;
+        int contador = 0;
+
+        while (contador < 10)
+        {
+            resultado = resultado + numero;
+            contador = contador + 1;
+        }
+
+        Console.WriteLine($"O resultado de {numero} x 10 é: {resultado}");
+    }
+}
