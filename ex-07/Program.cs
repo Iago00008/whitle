811682b70d57365412bbfc2b1834a06cc8c7af29
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-30 09:05:41 -0300

    07

--- ex-07/Program.cs
+++ ex-07/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int contador = 10;
+
+        Console.WriteLine("Contagem decrescente de 10 a 1:");
+        while (contador >= 1)
+        {
+            Console.WriteLine(contador);
+            contador = contador - 1;
+        }
+        contador = 1;
+
+        Console.WriteLine("Contagem crescente de 1 a 10:");
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador = contador + 1;
+        }
+    }
+}
