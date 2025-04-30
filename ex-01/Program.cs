1c920188b84cd0eb3fc791bb1042ff61260de147
Iago00008 <iagofazevedo.08@gmail.com>
2025-04-30 08:47:50 -0300

    01

---
+++ ex-01/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string frase = "Esta frase será impressa 10 vezes em C#.";
+        int contador = 1;
+
+        while (contador <= 10)
+        {
+            Console.WriteLine(frase);
+            contador = contador + 1;
+        }
+    }
+}
